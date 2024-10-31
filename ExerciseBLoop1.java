b394adf441b84724ee3fe7549b31443a1ff34e81
nafilafiryal <nafilahfiryal05@gmail.com>
2024-10-31 22:02:51 +0700

    Exercise b Loop 1

---
+++ ExerciseBLoop1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * ExerciseBLoop1
+ */
+public class ExerciseBLoop1 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int i=25;
+        while (i>=1) {
+            System.out.println(i);
+            i--;
+        }
+    }
+}
