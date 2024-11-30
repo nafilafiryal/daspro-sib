42b843078e8b64220099064ad6def58f02ce5813
nafilafiryal <nafilahfiryal05@gmail.com>
2024-11-30 21:27:33 +0700

    Assignment Function 1, 2a

--- Function1/Assignment2aFunction1.java
+++ Function1/Assignment2aFunction1.java
@@ -4,26 +4,24 @@ import java.util.Scanner;
 
 public class Assignment2aFunction1 {
     
-    static void  sales (String[] nameFruit, int[][] fruit) {
-        for (int i = 0; i < 7; i++) {
-            System.out.println("Day " + (i+1) + ": ");
-            for (int j = 0; j < nameFruit.length; j++) {
-                System.out.println(nameFruit[j] + ": " + fruit[j][i]);
+    static void  sales (String[] nameFruit, int[][] numberFruit) {
+        Scanner input = new Scanner(System.in);
+        for (int i = 0; i < nameFruit.length; i++) {
+            System.out.print("Fruit " + (i+1) + ": ");
+            nameFruit[i] = input.nextLine();
+            input.nextLine();
+            for (int j = 0; j < 7; j++) {
+                System.out.print("Day " + (j+1) + ": ");
+                numberFruit[i][j] = input.nextInt();
             }
             System.out.println();
         }
         
     }  
     public static void main(String[] args) {
-        String[] nameFruit = {"Apple", "Orange", "Grape", "Kiwi", "Guava"
-        };
-        int[][] fruit={
-            {20, 19, 25, 20, 10, 0, 10},
-            {30, 30, 40, 10, 15, 20, 25},
-            {5, 0, 20, 25, 10, 5, 45},
-            {50, 0, 7, 8, 0, 30, 60},
-            {15, 10, 16, 15, 10, 10, 5}
-        };
-        sales(nameFruit, fruit);
+        Scanner input = new Scanner(System.in);
+        String[] nameFruit = new String[5];
+        int[][] numberFruit = new int[5][7];
+        sales(nameFruit, numberFruit);
     }
 }
---
+++ Function1/Assignment2bFunction1.java
@@ -0,0 +1,5 @@
+package Function1;
+
+public class Assignment2bFunction1 {
+    
+}
